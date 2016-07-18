592c0d7d0276f16a3cf432c980753c7531590aeb
tboneteen18 <tboneteen18@gmail.com>
2016-07-18 13:58:11 -0600

    Add project files.

---
+++ Web_Services/Models/Weather.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web_Services;
+
+namespace Web_Services.Models
+{
+    public class Weather
+    {
+        public int WeatherID { get; set; }
+        public string Date { get; set; }
+        public string Desciption { get; set; }
+        public string MorningLow { get; set; }
+        public string DaytimeHigh { get; set; }
+    }
+}
